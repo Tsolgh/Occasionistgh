49022bf04301ca846ec6972291f895e3e18f8c0a
PhilipAp <pylphill@gmail.com>
2018-12-07 11:35:59 +0000

    initial push

---
+++ src/main/java/com/tsolgh/OccassionistghApplication.java
@@ -0,0 +1,12 @@
+package com.tsolgh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OccassionistghApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OccassionistghApplication.class, args);
+	}
+}
